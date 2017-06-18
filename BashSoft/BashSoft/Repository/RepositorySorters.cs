4f76936d58c4d0a02db6d25d3257d7d3e9d2d1b3
tanyta78 <tanyta78@gmail.com>
2017-06-18 14:05:36 +0300

    linq and folder structure of project

---
+++ BashSoft/BashSoft/Repository/RepositorySorters.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BashSoft
+{
+    public static class RepositorySorters
+    {
+        public static void OrderAndTake(Dictionary<string, List<int>> wantedData, string comparison, int studentsToTake)
+        {
+            comparison = comparison.ToLower();
+            switch (comparison)
+            {
+                case "ascending":
+                    PrintStudents(wantedData.OrderBy(x => x.Value.Sum())
+                        .Take(studentsToTake)
+                        .ToDictionary(pair => pair.Key, pair => pair.Value));
+                    break;
+
+                case "descending":
+                    PrintStudents(wantedData.OrderByDescending(x => x.Value.Sum())
+                             .Take(studentsToTake)
+                             .ToDictionary(pair => pair.Key, pair => pair.Value));
+                    break;
+
+                default:
+                    OutputWriter.DisplayException(ExceptionMessages.InvalidComparisonQuery);
+                    break;
+            }
+        }
+
+        private static void PrintStudents(Dictionary<string, List<int>> studentsSorted)
+        {
+            foreach (var pair in studentsSorted)
+            {
+                OutputWriter.PrintStudent(pair);
+            }
+        }
+    }
+}
