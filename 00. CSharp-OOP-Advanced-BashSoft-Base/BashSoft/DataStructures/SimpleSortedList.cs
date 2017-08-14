27a6058e21dda9c9b459797f1096f238303b5c9a
Tanyta <tanyta78@gmail.com>
2017-08-15 00:22:19 +0300

    bubble, selection and insertion sort edited

--- 00. CSharp-OOP-Advanced-BashSoft-Base/BashSoft/DataStructures/SimpleSortedList.cs
+++ 00. CSharp-OOP-Advanced-BashSoft-Base/BashSoft/DataStructures/SimpleSortedList.cs
@@ -41,6 +41,70 @@ namespace BashSoft.DataStructures
             this.innerCollection[size] = element;
             this.size++;
             Array.Sort(this.innerCollection, 0, this.size, this.comparison);
+            // BubbleSort(this.innerCollection, this.comparison);
+            //SelectionSort(this.innerCollection, this.comparison);
+            //InsertionSort(this.innerCollection, this.comparison);
+            //to do QuickSort
+        }
+
+        private void InsertionSort(T[] collection, IComparer<T> comparer)
+        {
+            var equalityComparer = comparer ?? Comparer<T>.Default;
+            for (var counter = 0; counter < collection.Length - 1; counter++)
+            {
+                var index = counter + 1;
+                while (index > 0)
+                {
+                    if (equalityComparer.Compare(collection[index - 1], collection[index]) > 0)
+                    {
+                        var temp = collection[index - 1];
+                        collection[index - 1] = collection[index];
+                        collection[index] = temp;
+                    }
+                    index--;
+                }
+            }
+        }
+
+        private void SelectionSort(T[] collection, IComparer<T> comparer)
+        {
+            T temp;
+
+            for (int i = 0; i < collection.Length - 1; i++)
+            {
+                for (int j = i; j < collection.Length - 1; j++)
+                {
+                    T first = collection[i];
+                    T other = collection[j + 1];
+
+                    if (comparer.Compare(first, other) < 0)
+                    {
+                        temp = collection[i];
+                        collection[i] = collection[j + 1];
+                        collection[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        private void BubbleSort(T[] collection, IComparer<T> comparer)
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                isRunning = false;
+                for (int j = 0; j < collection.Length - 1; j++)
+                {
+                    T first = collection[j];
+                    T other = collection[j + 1];
+                    if (comparer.Compare(first, other) > 0)
+                    {
+                        collection[j] = other;
+                        collection[j + 1] = first;
+                        isRunning = true;
+                    }
+                }
+            }
         }
 
         public void AddAll(ICollection<T> collection)
@@ -57,6 +121,8 @@ namespace BashSoft.DataStructures
             }
 
             Array.Sort(this.innerCollection, 0, this.size, this.comparison);
+            //BubbleSort(this.innerCollection, this.comparison);
+            //SelectionSort(this.innerCollection, this.comparison);
         }
 
         private void MultiResize(ICollection<T> collection)
@@ -69,6 +135,7 @@ namespace BashSoft.DataStructures
 
             T[] newCollection = new T[newSize];
             Array.Copy(this.innerCollection, newCollection, this.size);
+
             this.innerCollection = newCollection;
         }
 
